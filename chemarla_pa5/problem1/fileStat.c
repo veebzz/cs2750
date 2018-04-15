af9fbd5a228bfa9df3541972ad6cd1e788d217e5
Chemarla <vrc522@delmar.umsl.edu>
2018-04-15 17:52:52 -0500

    fileStat.c
    Now works for multiple arugments

--- chemarla_pa5/problem1/fileStat.c
+++ chemarla_pa5/problem1/fileStat.c
@@ -1,34 +1,39 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(int argc, char *argv[]){
 	FILE *filep;
 	char input;
-	char filename[100];
+	char prevInput = '\n';
+	//char filename[100];
 	int charCount=0, lineCount=0, wordCount=0;
 
+	for (int i = 1; i < argc; i++){
+	//strcpy(filename, argv[1]);
 
-	strcpy(filename, argv[1]);
+	filep = fopen(argv[i], "r");
 
-	filep = fopen(filename, "r");
-
-	if (filep)
-	{
-		while ((input = getc(filep)) != EOF){
-			if(input != ' ' && input != '\n'){
-				charCount++;
+		if (filep)
+		{
+			charCount=0, lineCount=0, wordCount=0;
+			while ((input = getc(filep)) != EOF){
+				
+				if(input != ' ' && input != '\n'){
+					charCount++;
+				}
+				if(input ==' '&& prevInput != ' '){
+					wordCount++;
+				}
+				prevInput = input;
+				if(input == '\n'){
+					lineCount++;
+				}
 			}
-			if(input ==' ' || input =='\n'){
-				wordCount++;
-			}
-			if(input == '\n'){
-				lineCount++;
-			}
-		}
-		if(charCount > 0){
-			lineCount++;
-			wordCount++;
-		}
+	//	if(charCount > 0){
+	//		lineCount++;
+	//		wordCount++;
+	//	}
 	}
 	else	
 	{
@@ -38,7 +43,9 @@ int main(int argc, char *argv[]){
 	printf("Words: %d \n", wordCount);
 	printf("Lines: %d \n", lineCount);
 	getchar();
+	}
 	return 0;
+
 }
 
 
