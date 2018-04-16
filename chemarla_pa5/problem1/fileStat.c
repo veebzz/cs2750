aba2a71d0a2b94eec55a9b9f3d33e976a8b3f387
Chemarla <vrc522@delmar.umsl.edu>
2018-04-16 15:41:12 -0500

    finished fileStat.c

--- chemarla_pa5/problem1/fileStat.c
+++ chemarla_pa5/problem1/fileStat.c
@@ -6,44 +6,69 @@
 
 int count_nonspace();
 int count_word();
+char str[100];
+int tChar=0, tWord=0, tLine=0;//total stat variables
 int main(int argc, char *argv[]){
 	FILE *filep;
 	char input;
 	char prevInput = '\n';
 
 	int charCount=0, lineCount=0, wordCount=0;
-
-	for (int i = 1; i < argc; i++){
-
-	filep = fopen(argv[i], "r");
-
-		if (filep)
-		{
-		printf("File: %s\n", argv[i]);
-			charCount=0, lineCount=0, wordCount=0;
-			while ((input = getc(filep)) != EOF){
+		for (int i = 1; i < argc; i++){
+			// open file
+			filep = fopen(argv[i], "r");
+			//if the open file exits in the directory 
+			if (filep)
+			{
+				charCount=0, lineCount=0, wordCount=0;
+				//while loop that counts the characters, words, and lines
+				while ((input = getc(filep)) != EOF){
 				
-				if(input != ' ' && input != '\n'){
-					charCount++;
+					if((input == ' '&& prevInput == ' ')||(input == '\n' && prevInput == '\n')||(input == '\t'&& prevInput == '\t')){
+						continue;	//skip iteration
+					}
+					if(input ==' '&& prevInput != ' '){
+						wordCount++;
+						tWord++;
+					}
+					if( input != ' ' && prevInput == '\n'){
+						wordCount++;
+						tWord++;
+					}
+					if((input != ' ')&&( input != '\n')&&(input != '\t')){
+					 	charCount++;
+						tChar++;
+				       	}
+					
+					//let preInput equal input for next iteration
+					prevInput = input;
+					//purposely chose to only counts lines with text
+					//project pdf doesnt specify that it has imitate wc command which counts
+					//empty lines(wc command is just a suggestion)
+					if(input == '\n'){
+						lineCount++;
+						tLine++;
+					}
+
 				}
-				if((input == ' '&& prevInput == ' ')||(input == '\n' && prevInput == '\n')){
-					continue;
-				}
-				if(input ==' '&& prevInput != ' '){
-					wordCount++;
-				}
-				if(input != ' ' && prevInput == '\n'){
-					wordCount++;
-				}
-				prevInput = input;
-				if(input == '\n'){
-					lineCount++;
-				}
+				//add 1 to word count because of a EOF issue
+				wordCount++;
+				tWord++;
+				//Display each file's stat	
+				printf("File: %s\n",argv[i]);
+				printf("Characters: %d \n",charCount);
+				printf("Words: %d \n", wordCount);
+				printf("Lines: %d \n", lineCount);
+				printf("---------------------------------\n");
+
 			}
-		}
-		else if(!filep)	
-		{
-			char str[100];
+		
+
+	//given argument doesnt exist
+	//ask user for sentence to read from stdio stream
+	if(!filep)
+	{
+			
 			printf("Failed to find/open file\n");
 			printf("Enter a sentence: ");
 			fgets(str, sizeof(str), stdin);
@@ -51,20 +76,38 @@ int main(int argc, char *argv[]){
 			printf("Characters:  %d\n", count_nonspace(str));
 			printf("Words: %i\n", count_word(str));
 			printf("Lines: 1\n");
+			//end for main funcition if the user chose to input a sentence
 			return 0;
 				
 		}
 	}
+	//if no command line arguments given ask user for sentence
+	if(argc < 2)
+	{
+			
+			printf("No command line arguments given\n");
+			printf("Enter a sentence: ");
+			fgets(str, sizeof(str), stdin);
 		
-	printf("Characters: %d \n",charCount);
-	printf("Words: %d \n", wordCount);
-	printf("Lines: %d \n", lineCount);
-	
-	
+			printf("Characters:  %d\n", count_nonspace(str));
+			printf("Words: %i\n", count_word(str));
+			printf("Lines: 1\n");
+			//end for main funcition if the user chose to input a sentence
+			return 0;
+				
+		}
+	//if 1 or more command line arguments given display total stats at the end	
+	if(argc > 1){
+	//Display total statistic at last iteration of for loop
+	printf("Total Statistics\n");
+	printf("Total Characters: %d \n",tChar);
+	printf("Total Words: %d \n", tWord);
+	printf("Total Lines: %d \n", tLine);
+	}
 	return 0;
 
 }
-
+// function to count characters if user input a sentence
 int count_nonspace(const char* str){
 	int ccount =0;
 
@@ -74,7 +117,7 @@ int count_nonspace(const char* str){
 	}
 		return ccount;
 }
-
+//function to count words if user inputs a sentence
 int count_word(const char* str){
 	int wcount = 0;
 	int len = 0;
