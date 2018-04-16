f7bbb0128530b5e17665893699b7ecdb39398906
Chemarla <vrc522@delmar.umsl.edu>
2018-04-15 20:22:11 -0500

    fileStat.c
    Character count for stdstream is working, need to do word count

--- chemarla_pa5/problem1/fileStat.c
+++ chemarla_pa5/problem1/fileStat.c
@@ -1,28 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
+
+int count_nonspace();
+int count_word();
 int main(int argc, char *argv[]){
 	FILE *filep;
 	char input;
 	char prevInput = '\n';
-	//char filename[100];
+
 	int charCount=0, lineCount=0, wordCount=0;
 
 	for (int i = 1; i < argc; i++){
-	//strcpy(filename, argv[1]);
 
 	filep = fopen(argv[i], "r");
 
 		if (filep)
 		{
+		printf("File: %s\n", argv[i]);
 			charCount=0, lineCount=0, wordCount=0;
 			while ((input = getc(filep)) != EOF){
 				
 				if(input != ' ' && input != '\n'){
 					charCount++;
 				}
+				if((input == ' '&& prevInput == ' ')||(input == '\n' && prevInput == '\n')){
+					continue;
+				}
 				if(input ==' '&& prevInput != ' '){
+					wordCount++;
+				}
+				if(input != ' ' && prevInput == '\n'){
 					wordCount++;
 				}
 				prevInput = input;
@@ -30,22 +40,63 @@ int main(int argc, char *argv[]){
 					lineCount++;
 				}
 			}
-	//	if(charCount > 0){
-	//		lineCount++;
-	//		wordCount++;
-	//	}
+		}
+		else	
+		{
+			char str[100];
+			printf("Failed to open file\n");
+			printf("Enter a sentence: ");
+			fgets(str, sizeof(str), stdin);
+		
+			printf("charcount:  %d\n", count_nonspace(str));
+			printf("wcount: %d\n", count_word(str));
+		//	for(int i = 0; i < strlen(p); i++){
+		//		if(isspace(p[i])){
+		//			continue;
+		//		}
+		//		else if(isaplha(p[i])){
+		//			charCount++;
+		//		}
+		//		else if(!isspace(p[i])&& isspace(p[i-1])){
+		//			wordCount++;
+		//		}
+				
+			
+		//	}
+		//	lineCount =1;
+			 //printf("Characters: %d \n",charCount);
+			// printf("Words: %d \n", wordCount);
+			// printf("Lines: %d \n", lineCount);
+			// return 0;
+			break;
+				
+		}
 	}
-	else	
-	{
-	printf("Failed to open file\n");
-	}
+		
 	printf("Characters: %d \n",charCount);
 	printf("Words: %d \n", wordCount);
 	printf("Lines: %d \n", lineCount);
 	getchar();
-	}
+	
 	return 0;
 
 }
 
+int count_nonspace(const char* str){
+	int ccount =0;
 
+	while(*str){
+		if(!isspace(*str++))
+			ccount++;
+	}
+		return ccount;
+}
+int count_word(const char* str){
+	int wcount = 0;
+	while(*str){
+		if(isspace(*str) && !isspace(*str++)){
+		wcount++;
+		}
+	}
+	return wcount;
+}	
