fa9b8198c659a0ec4e7f7afa8482bcad4adcc1f9
Chemarla <vrc522@delmar.umsl.edu>
2018-04-24 15:32:12 -0500

    fgets portion

--- chemarla_pa6/runsim.c
+++ chemarla_pa6/runsim.c
@@ -10,8 +10,11 @@
 
 int main (int argc, char *argv[]){
 	pid_t childpid = 0;
+	pid_t wpid;
 	int pr_limit;
-	int pr_count;
+	int pr_count= 0;
+	int status = 0;
+	char fArr[15];
 
 	//check if user entered two arguments
 	if (argc != 2){
@@ -21,13 +24,17 @@ int main (int argc, char *argv[]){
 
 	//pr_limit maximum number of children allowed to execute at a time
 	pr_limit = atoi(argv[1]);
-	pr_count = 0;
-	
-	//use fgets to open up to MAX BUFFER ?? 
-	for(i = 0; i < n; i ++)
-		if((childpid = fork()) <= 0)
+
+
+	// if count is limit wait for  child to finish
+	for(i = 0; i < n; i ++){
+		if((childpid = fork()) <= 0){
 			break;
-	
+	}
+	while((wpid = wait(&status)) > 0);
+	//fgets command line up to MAX_BUF char
+	fgets(fArr, 15, stdin);
+
 	fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n",i, getpid(), getppid(), childpid);
 	
 	return 0;
