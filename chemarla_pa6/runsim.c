8379c8bfdb70ed1957e48dd340aa886aae631455
Chemarla <vrc522@delmar.umsl.edu>
2018-04-24 14:29:01 -0500

    commit 2

--- chemarla_pa6/runsim.c
+++ chemarla_pa6/runsim.c
@@ -4,24 +4,28 @@
  */
 
 #include <stdio.h>
-#include <stdlin.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
 
 int main (int argc, char *argv[]){
-	pid_t child = 0;
-	int i, n;
+	pid_t childpid = 0;
+	int pr_limit;
+	int pr_count;
 
 	if (argc != 2){
 		fprintf(stderr, "Usage: %s processes \n", argv[0]);
 		return 1;
 	}
-	n = atoi(argv[1]);
+	//pr_limi- maximum number of children allowed to execute at a time
+	pr_limit = atoi(argv[1]);
+	pr_count = 0;
+	
 	for(i = 0; i < n; i ++)
 		if((childpid = fork()) <= 0)
 			break;
 	
-	fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n",
-		i, getpid(), getppid(), childpid);
+	fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n",i, getpid(), getppid(), childpid);
 	
 	return 0;
 }
