92f1ca5a57f34ad7ec0dbe11d121de1b6806d3d4
Chemarla <vrc522@delmar.umsl.edu>
2018-04-30 17:01:38 -0500

    processes working

--- chemarla_pa6/runsim.c
+++ chemarla_pa6/runsim.c
@@ -9,14 +9,17 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+#define MAX_BUF 50
+
+
 int main (int argc, char *argv[]){
 	pid_t childpid = 0;
 	pid_t wpid;
 	int pr_limit;
 	int pr_count= 0;
 	int status = 0;
-	char fArr[15];
-	int i = 0;
+	char fArr[MAX_BUF];
+	int i;
 	//check if user entered two arguments
 	if (argc != 2){
 		fprintf(stderr, "Usage: %s processes \n", argv[0]);
@@ -26,21 +29,25 @@ int main (int argc, char *argv[]){
 	//pr_limit maximum number of children allowed to execute at a time
 	pr_limit = atoi(argv[1]);
 
-
-	// if count is limit wait for  child to finish
-	for(i = 1; i < pr_limit; i ++){
-		if((childpid = fork()) <= 0)
-			break;
-	
-	//while((wpid = wait(&status)) > 0);
-		if(childpid = waitpid(-1, &status, WNOHANG)> 0){
-			printf("Child %d terminated\n", childpid);
+	//main loop	
+	while(fgets(fArr, MAX_BUF -1, stdin) !=NULL)
+	{
+		//if prrcount = prlimit wat for child to finish an ddecrement pr_count
+		if(pr_count == pr_limit)
+		{
+			wait(NULL);
 			pr_count--;
 		}
+		if((childpid = fork())){
+			system(fArr);
+		}
+		if(childpid = waitpid(-1, &status, WNOHANG)> 0){
+                //         printf("Child %d terminated\n", childpid);
+	                   pr_count--;
+              	}		  
 	}
-	//fgets command line up to MAX_BUF char
-	fgets(fArr, 15, stdin);
 
+		 
 	fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n",i, getpid(), getppid(), childpid);
 	
 	return 0;
--- chemarla_pa6/testsim.c
+++ chemarla_pa6/testsim.c
@@ -1,7 +1,6 @@
 /*testsim.c*/
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+
 int main(int argc, char *argv[])
 {
 	int n;
@@ -13,11 +12,16 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
-	scanf(argv[1], "%d", &sec);
-	scanf(argv[2], "%d", &n);
-
-	while(n-- > 0)
+	sscanf(argv[1], "%d", &sec);
+	sscanf(argv[2], "%d", &n);
+	
+	for(int i = n; i > 0; i--){
+		printf("[Repeat: %d] Process: %d is sleeping for %d seconds\n",i,getpid(),sec);
 		sleep(sec);
-	printf("Process ID = %d \n", getpid());
+	}
+
+	printf("Child Process: %d terminated\n",getpid());
+	
+	//printf("Child Process ID = %d \n", getpid());
 	return sec;
 }
